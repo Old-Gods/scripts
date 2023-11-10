2bf472449714a49186a9d8112ce3e5c3f0a3ff39
John Wright <jrite@protonmail.com>
2023-11-10 20:43:43 +0000

    feat: add ship command

--- src/cli/bandcamp/run.ts
+++ src/cli/bandcamp/run.ts
@@ -2,5 +2,11 @@ import { runExit } from 'clipanion'
 import { BandcampLoginCommand } from './commands/BandcampLoginCommand'
 import { BandcampBandsCommand } from './commands/BandcampBandsCommand'
 import { BandcampOrdersCommand } from './commands/BandcampOrdersCommand'
+import { BandcampShipCommand } from './commands/BandcampShipCommand'
 
-runExit([BandcampLoginCommand, BandcampBandsCommand, BandcampOrdersCommand])
+runExit([
+  BandcampLoginCommand,
+  BandcampBandsCommand,
+  BandcampOrdersCommand,
+  BandcampShipCommand,
+])
